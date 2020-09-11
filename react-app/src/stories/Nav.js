3300b7a4add3b8cfce72d77adf949917ec509f96
Tyler Krys <Tyler.Krys@gov.bc.ca>
2020-09-11 16:33:40 -0700

    Add Nav component

---
+++ react-app/src/stories/Nav.js
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./nav.css";
+
+export const Nav = ({ links, dark }) => (
+  <nav
+    className = { dark ? "nav nav--dark" : "nav" }
+  >
+    <ul>
+      {links.map((link) =>
+        <li><a href="">{link}</a></li>
+      )}
+    </ul>
+  </nav>
+);
+
+Nav.propTypes = {
+  dark: PropTypes.bool,
+  links: PropTypes.array
+};
+
+Nav.defaultProps = {
+  dark: false,
+  links: [],
+};
---
+++ react-app/src/stories/Nav.stories.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import { Nav } from "./Nav";
+
+export default {
+  title: "Example/Nav",
+  component: Nav,
+  argTypes: {
+    dark: { control: "boolean" },
+  },
+};
+
+const Template = (args) => <Nav {...args} />;
+
+export const Standard = Template.bind({});
+Standard.args = {
+  links: ['Services', 'Topics', 'News', 'Public Engagements', 'Jobs & MyHR', 'Get Help']
+};
+
+export const Dark = Template.bind({});
+Dark.args = {
+  dark: true,
+  links: ['Services', 'Topics', 'News', 'Public Engagements', 'Jobs & MyHR', 'Get Help']
+};
