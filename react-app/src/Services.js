67b523fafe9993ea35d8369864a30561a33a3091
Tyler Krys <Tyler.Krys@gov.bc.ca>
2020-09-22 08:53:28 -0700

    Add filtering to Services page

--- react-app/src/Services.js
+++ react-app/src/Services.js
@@ -6,28 +6,37 @@ import { ServiceHighlight } from "./stories/ServiceHighlight";
 
 function Services() {
   const [services, setServices] = useState({});
+  const [inputValue, setInputValue] = useState('');
 
   async function fetchData() {
     const res = await fetch('/api/services');
     res
       .json()
-      .then(res => setServices(res.services))
-      .catch(err => console.log(err))
+      .then((res) => setServices(res.services))
+      .catch((err) => console.log(err));
   }
 
   useEffect(() => {
     fetchData();
   }, []);
 
+  const onChangeHandler = (event) => {
+    setInputValue(event.target.value);
+  };
+
   return (
     <div className="div--services-container">
-
       {/* Search bar */}
       <div className="div--search">
         <h2>Find a program or service</h2>
         <form>
           <label>
-            <input></input>
+            <input
+              type="text"
+              name="search"
+              onChange={onChangeHandler}
+              value={inputValue}
+            />
           </label>
           <button></button>
         </form>
@@ -38,8 +47,12 @@ function Services() {
         {services &&
           services.length > 0 &&
           services
-            .filter(service => service.highlight === true)
-            .map(({name, url, external, image}, index) => (
+            .filter(
+              (service) =>
+                service.highlight === true &&
+                service.name.toLowerCase().includes(inputValue.toLowerCase())
+            )
+            .map(({ name, url, external, image }, index) => (
               <ServiceHighlight
                 key={index && ` - ` && name}
                 name={name}
@@ -47,20 +60,29 @@ function Services() {
                 url={url}
                 external={external}
               />
-        ))}
+            ))}
       </div>
 
       {/* Services list */}
-      {services && services.length > 0 && services.map(({name, description, url, external}, index) => (
-        <Service
-          key={index}
-          name={name}
-          description={description}
-          url={url}
-          external={external}
-        />
-      ))}
-
+      {services &&
+        services.length > 0 &&
+        services
+          .filter(
+            (service) =>
+              service.name.toLowerCase().includes(inputValue.toLowerCase()) ||
+              service.description
+                .toLowerCase()
+                .includes(inputValue.toLowerCase())
+          )
+          .map(({ name, description, url, external }, index) => (
+            <Service
+              key={index}
+              name={name}
+              description={description}
+              url={url}
+              external={external}
+            />
+          ))}
     </div>
   );
 }
