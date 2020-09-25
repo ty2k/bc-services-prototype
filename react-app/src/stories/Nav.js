fd3fa700f332d2cae4f91e08cbcf0016ca81b77a
Tyler Krys <Tyler.Krys@gov.bc.ca>
2020-09-24 17:50:47 -0700

    Add Nav toggle function in Header component

--- react-app/src/stories/Nav.js
+++ react-app/src/stories/Nav.js
@@ -1,20 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
+import MediaQuery from "react-responsive";
 
+import { Button } from "./Button";
+import { SearchBar } from "./SearchBar";
 import "./nav.css";
 
 export const Nav = ({ dark, hidden, links }) => (
   <nav
-    className = {`${dark ? "nav nav--dark" : "nav"} ${hidden ? "nav--hidden" : ""}`}
+    className={`${dark ? "nav nav--dark" : "nav"} ${
+      hidden ? "nav--hidden" : ""
+    }`}
   >
+    <MediaQuery maxWidth={991}>
+      <SearchBar />
+      <div className="nav-container--buttons">
+        <Button size="medium" label="Login" />
+        <Button primary size="medium" label="Register" />
+      </div>
+    </MediaQuery>
     <ul>
-      {links.map((link) =>
+      {links.map((link) => (
         <li key={link}>
-          <a href="/#">
-            {link}
-          </a>
+          <a href="/#">{link}</a>
         </li>
-      )}
+      ))}
     </ul>
   </nav>
 );
@@ -22,11 +32,18 @@ export const Nav = ({ dark, hidden, links }) => (
 Nav.propTypes = {
   dark: PropTypes.bool,
   hidden: PropTypes.bool,
-  links: PropTypes.array
+  links: PropTypes.array,
 };
 
 Nav.defaultProps = {
   dark: false,
   hidden: false,
-  links: ['Programs & Services', 'Topics', 'News', 'Public Engagements', 'Jobs & MyHR', 'Get Help'],
+  links: [
+    "Programs & Services",
+    "Topics",
+    "News",
+    "Public Engagements",
+    "Jobs & MyHR",
+    "Get Help",
+  ],
 };
